98563f31632b0d3801288c43de275163277f7692
mahdee18 <mahdeec10@gmail.com>
2023-04-12 19:54:54 +0600

    Create Footer part

---
+++ README.md
@@ -0,0 +1,12 @@
+# Website Features
+
+- featured jobs 
+- single job details page 
+- statistics of assignment
+- applid jobs single page 
+- multiple filters options 
+- show more button
+- article page 
+
+# Website Live Link
+[job-Mart](https://job-mart.netlify.app/)
--- src/components/Statistic/Statistic.jsx
+++ src/components/Statistic/Statistic.jsx
@@ -11,6 +11,9 @@ import {
   Legend,
 } from 'recharts';
 
+import Banner from '../../assets/All Images/Vector.png';
+import Banner1 from '../../assets/All Images/Vector-1.png';
+
 const data = [
   { name: 'Assignment 1', score: 60, netMarks: 60 },
   { name: 'Assignment 2', score: 60, netMarks: 60 },
@@ -25,37 +28,45 @@ const data = [
 class Statistic extends PureComponent {
   render() {
     return (
-      <div style={{ width: '100%', height: 300 }} className='my-52'>
-        <ResponsiveContainer>
-          <ComposedChart
-            data={data}
-            margin={{ top: 20, right: 20, bottom: 20, left: 20 }}
-          >
-            <CartesianGrid stroke="#f5f5f5" />
-            <XAxis dataKey="name" />
-            <YAxis
-              label={{
-                value: 'Assignment Score',
-                angle: -90,
-                position: 'insideLeft',
-              }}
-              tickFormatter={(value) => `${value}`}
-              domain={[0, 100]}
-            />
-            <Tooltip
-              formatter={(value) => `${value}`}
-              labelFormatter={(label) => ` ${label}`}
-            />
-            <Legend />
-            <Bar dataKey="score" name="Score" fill="#8884d8" />
-            <Line
-              type="monotone"
-              dataKey="netMarks"
-              name="Net Marks"
-              stroke="#82ca9d"
-            />
-          </ComposedChart>
-        </ResponsiveContainer>
+      <div>
+        <div className="h-[298px] w-full relative flex justify-center items-center">
+          <img src={Banner1} className="absolute top-0 right-0" />
+          <h1 className="text-[32px] font-bold">Statistics</h1>
+          <img src={Banner} className="absolute bottom-0 left-0" />
+        </div>
+
+        <div style={{ width: '100%', height: 300 }} className='my-36'>
+          <ResponsiveContainer>
+            <ComposedChart
+              data={data}
+              margin={{ top: 20, right: 20, bottom: 20, left: 20 }}
+            >
+              <CartesianGrid stroke="#f5f5f5" />
+              <XAxis dataKey="name" />
+              <YAxis
+                label={{
+                  value: 'Assignment Score',
+                  angle: -90,
+                  position: 'insideLeft',
+                }}
+                tickFormatter={(value) => `${value}`}
+                domain={[0, 100]}
+              />
+              <Tooltip
+                formatter={(value) => `${value}`}
+                labelFormatter={(label) => ` ${label}`}
+              />
+              <Legend />
+              <Bar dataKey="score" name="Score" fill="#8884d8" />
+              <Line
+                type="monotone"
+                dataKey="netMarks"
+                name="Net Marks"
+                stroke="#82ca9d"
+              />
+            </ComposedChart>
+          </ResponsiveContainer>
+        </div>
       </div>
     );
   }
